56154a242cc495c495afbcd782db2fe450ce557a
aiqawa1266 <airaaaa803@gmail.com>
2025-07-31 08:46:55 +0700

    Update app.js
    mengubah beberapa code

--- app.js
+++ app.js
@@ -2,17 +2,42 @@ new Vue({
   el: "#app",
   data: {
     tugasBaru: "",
-    daftarTugas: [],
+    daftarTugas: [
+      { nama: "Belajar Vue", selesai: false },
+      { nama: "Bikin kopi", selesai: false },
+    ],
   },
   methods: {
     tambahTugas() {
       if (this.tugasBaru.trim() !== "") {
-        this.daftarTugas.push(this.tugasBaru);
+        this.daftarTugas.push({
+          nama: this.tugasBaru,
+          selesai: false,
+        });
         this.tugasBaru = "";
+        this.simpanKeLocalStorage();
       }
     },
     hapusTugas(index) {
       this.daftarTugas.splice(index, 1);
+      this.simpanKeLocalStorage();
+    },
+    simpanKeLocalStorage() {
+      localStorage.setItem("daftarTugas", JSON.stringify(this.daftarTugas));
+    },
+  },
+  mounted() {
+    const data = localStorage.getItem("daftarTugas");
+    if (data) {
+      this.daftarTugas = JSON.parse(data);
+    }
+  },
+  watch: {
+    daftarTugas: {
+      handler() {
+        this.simpanKeLocalStorage();
+      },
+      deep: true,
     },
   },
 });
